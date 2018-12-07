5d5c14fdb68bc17f5bc7d8699354915595b50c81
Granit23 <granitarifi.23@gmail.com>
2018-12-07 11:50:50 -0500

    refactors code and adds button to randomize css background gradient

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var h3 = document.querySelector("h3");
+var btn = document.getElementById("randBtn");
 
 // match css background with color picker on page load 
-body.style.background = 
-    "linear-gradient(to right, " 
-    + color1.value + ", " 
-    + color2.value + ")";
-css.textContent = body.style.background + ";";
-
+setGradient();
 
 function setGradient() {
     body.style.background = 
@@ -19,10 +15,31 @@ function setGradient() {
     + color2.value + ")";
 
     css.textContent = body.style.background + ";";
-    // h3.innerHTML = "Color 1: " + color1.value + "<br>Color 2: " + color2.value;
+}
+
+
+//from stackOverflow
+function getRandomColor()
+{
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function randGen() {
+   
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    setGradient();
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+btn.addEventListener("click", randGen);
 
